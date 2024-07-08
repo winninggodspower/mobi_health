9f75f52e801eb3d0957ee837cd5dc7e3ef36e063
winnning godspower <winnninggodspower@gmail.com>
2024-07-08 07:19:22 +0100

    fix steps icon

--- lib/svg_assets.dart
+++ lib/svg_assets.dart
@@ -3,12 +3,13 @@ const String sleepCircleIcon = 'assets/svgs/sleep-circle.svg';
 const String lightBulbIcon = 'assets/svgs/lightbulb-on.svg';
 const String heartCircleIcon = 'assets/svgs/heart-circle.svg';
 const String heartIcon = 'assets/svgs/heart.svg';
-const String temperatureCircleIcon = 'assets/svgs/body_temperature_circle.svg';
-const String temperatureIcon = 'assets/svgs/body_temperature.svg';
-const String weightCircleIcon = 'assets/svgs/weight.svg';
-const String stepsCircleIcon = 'assets/svgs/steps-circle.png';
+const String temperatureIcon = 'assets/svgs/weight.svg';
+const String temperatureCircleIcon = 'assets/svgs/body_temperature.svg';
+const String weightCircleIcon = 'assets/svgs/body_temperature_circle.svg';
+const String stepsCircleIcon = 'assets/svgs/steps-circle.svg';
 
 const String homeIcon = 'assets/svgs/Home.svg';
 const String customerSupportIcon = 'assets/svgs/customer-support.svg';
 const String deviceIcon = 'assets/svgs/device.svg';
 const String bookIcon = 'assets/svgs/icon-park-outline_read-book.svg';
+
