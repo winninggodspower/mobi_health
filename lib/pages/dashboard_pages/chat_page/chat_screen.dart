98cca21305a987b3005c81533d867a2ebcaea083
winnning godspower <winnninggodspower@gmail.com>
2024-07-24 14:28:59 +0100

    add chat bubble

--- lib/pages/dashboard_pages/chat_page/chat_screen.dart
+++ lib/pages/dashboard_pages/chat_page/chat_screen.dart
@@ -1,10 +1,8 @@
+import 'dart:ffi';
+
 import 'chat_widget.dart';
 import '../action_dropDown/export_action_drop_down.dart';
 import 'package:mobi_health/svg_assets.dart';
-
-
-
-// bool isSignalClick = false;
 
 
 class ChatScreen extends StatefulWidget {
@@ -19,6 +17,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColors.backgroundColor,
+        // bottomNavigationBar: ,
         body: SafeArea(
             child: Container(
                 // color: Colors.red,
@@ -91,10 +90,74 @@ class _ChatScreenState extends State<ChatScreen> {
                       const SizedBox(
                         height: 10,
                       ),
+                      const    Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            ChatBubble(
+                              isMe: true,
+                              message: 'hey i\'m sick what do you want to do about it dfad dfasdf dfasdf asdfas',
+                              ),
+                            SizedBox(height: 20,),
+                            ChatBubble(
+                              message: 'hey i\'m sick what do you want to do about it dfad dfasdf dfasdf asdfas',
+                              ),
+                          ],
+                        ),
+                      ),
                       const ChatBox()
                     ],
                   ),
                 ))));
+  }
+}
+
+class ChatBubble extends StatelessWidget {
+  final bool isMe;
+  final String message;
+
+  const ChatBubble({
+    super.key,
+    required this.message,
+    this.isMe = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double maxWidth = constraints.maxWidth * 0.7;
+        return Row(
+          mainAxisAlignment: isMe ? MainAxisAlignment.end: MainAxisAlignment.start,
+          children: [
+            Container(
+              constraints: BoxConstraints(
+                maxWidth: maxWidth, // Set the maximum width to 70% of the screen width
+              ),
+              decoration: BoxDecoration(
+                color: AppColors.primary_200Color,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 14, vertical: 8),
+              child: Text(
+                message,
+                softWrap: true,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(
+                      fontSize: 14,
+                      color: AppColors.primary_800Color,
+                      fontWeight: FontWeight.w700,
+                    ),
+                textAlign: isMe? TextAlign.end: TextAlign.start,
+              ),
+            ),
+          ],
+        );
+      }
+    );
   }
 }
 
