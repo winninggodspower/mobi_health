57109bb14628273162755c1fbac3498571f139a5
winnning godspower <winnninggodspower@gmail.com>
2024-07-25 10:57:35 +0100

    remove workmanager conditon

--- lib/pages/dashboard_pages/chat/report_emergency_popup.dart
+++ lib/pages/dashboard_pages/chat/report_emergency_popup.dart
@@ -5,10 +5,10 @@ import 'package:mobi_health/theme.dart';
 import 'package:mobi_health/widgets/app_buttons.dart';
 import 'package:mobi_health/providers/authentication_provider.dart';
 import 'package:mobi_health/providers/health_data_provider.dart';
+import 'package:twilio_flutter/twilio_flutter.dart';
+import 'package:mobi_health/services/twillio_service.dart';
 import 'package:mobi_health/util.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
-
 
 class ReportEmergencyPopup extends StatefulWidget {
   const ReportEmergencyPopup({super.key});
@@ -34,71 +34,75 @@ class _ReportEmergencyPopupState extends State<ReportEmergencyPopup> {
       child: Padding(
         padding: const EdgeInsets.all(15),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'What is the emergency ?',
-                style: GoogleFonts.openSans(
-                  fontSize: 15,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'What is the emergency ?',
+                  style: GoogleFonts.openSans(
+                    fontSize: 15,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ),
-              SizedBox(height: 1.h),
-              EmergencyInput(
-                borderColor: AppColors.secondaryColor,
-                hintText: 'Whats your emergency?',
-                textInput: _emergencyController,
-                textType: TextInputType.text,
-              ),
-              SizedBox(height: 0.02.dp),
-              SizedBox(
-                width: double.infinity,
-                height: 6.h,
-                child: Row(
-                  children: [
-                    Checkbox(
-                      value: _isSelected,
-                      onChanged: (value) {
-                        setState(() {
-                          _isSelected = value ?? false;
-                        });
-                      },
-                      activeColor: AppColors.secondaryColor,
-                      checkColor: AppColors.backgroundColor,
-                    ),
-                    Text(
-                      'Notify my emergency contact',
-                      style: GoogleFonts.openSans(
-                        fontSize: 13,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w400,
+                SizedBox(height: 1.h),
+                EmergencyInput(
+                  borderColor: AppColors.secondaryColor,
+                  hintText: 'Whats your emergency?',
+                  textInput: _emergencyController,
+                  textType: TextInputType.text,
+                ),
+                SizedBox(height: 0.02.dp),
+                SizedBox(
+                  width: double.infinity,
+                  height: 6.h,
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        value: _isSelected,
+                        onChanged: (value) {
+                          setState(() {
+                            _isSelected = value ?? false;
+                          });
+                        },
+                        activeColor: AppColors.secondaryColor,
+                        checkColor: AppColors.backgroundColor,
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Notify my emergency contact',
+                        style: GoogleFonts.openSans(
+                          fontSize: 13,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 1.h),
-              materialButton(
-                  buttonBkColor: AppColors.secondaryColor,
-                  onPres: () {
-                    print('pelar is the king of tiktok');
-                    if(_formKey.currentState?.validate() ?? true){
-                      _sendEmergencyMessage(_emergencyController.text);
-                    }else{
-                      print('invalid');
-                    }
-                  },
-                  size: 14,
-                  text: "Signal Emergency",
-                  textColor: AppColors.backgroundColor,
-                  width: 43.w,
-                  borderRadiusSize: 5,
-                  height: 6.h)
-            ],
-          ),
+                SizedBox(height: 1.h),
+                materialButton(
+                    buttonBkColor: AppColors.secondaryColor,
+                    onPres: () {
+                      print('pelar is the king of tiktok');
+                      if(_formKey.currentState?.validate() ?? true){
+                        print('valid');
+                        _sendEmergencyMessage(_emergencyController.text);
+                      }else{
+                        print('invalid');
+                      }
+                    },
+                    size: 14,
+                    text: "Signal Emergency",
+                    textColor: AppColors.backgroundColor,
+                    width: 43.w,
+                    borderRadiusSize: 5,
+                    height: 6.h)
+              ],
+            ),
+            )
         ),
       ),
     );
@@ -111,35 +115,21 @@ class _ReportEmergencyPopupState extends State<ReportEmergencyPopup> {
     String? emergencyContact = authProvider.userInfo?['emergencyContact'];
     if(emergencyContact == null || emergencyContact.isEmpty) {
       ShowSnackBar(context, 'you haven\'t added an emergency contact');
+      return;
     }
-
-    final url = Uri.parse('https://mubi-health-func.vercel.app/sendEmergencyContact');
 
     final Map<String, dynamic> data = {
       "userData": authProvider.userInfo,
       "healthVitals": healthProvider.healthData,
-      "emergencyContact": authProvider.userInfo?['emergencyContact'],
+      "emergencyContact": emergencyContact,
       "message": emergencyMessage,
     };
 
-    try {
-      final response = await http.post(
-        url,
-        headers: {'Content-Type': 'application/json',},
-        body: json.encode(data),
-      );
+    TwilioResponse response = await twilioFlutter.sendSMS(
+      toNumber : emergencyContact,
+      messageBody : 'hello world'
+    );
 
-      if (response.statusCode == 200) {
-        // Request was successful
-        print('Success: ${response.body}');
-      } else {
-        // Request failed
-        print('Failed: ${response.statusCode}');
-      }
-    } catch (e) {
-      // Error occurred
-      print('Error: $e');
-    }
   }
 
 }
---
+++ lib/services/twillio_service.dart
@@ -0,0 +1,7 @@
+import 'package:twilio_flutter/twilio_flutter.dart';
+
+final TwilioFlutter twilioFlutter = TwilioFlutter(
+    accountSid: 'AC5d1d1dde0ebd3372bfe2a90c321f7d99', 
+    authToken: 'df4c828d18175d73503df50438a11a11', 
+    twilioNumber: '+237673986209' 
+);
